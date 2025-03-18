c4321cf142e9e36e2ed1b4db37b9b50e2fa99c00
AHMAD9853 <iftekharahmad8758@gmail.com>
2025-03-19 00:29:50 +0530

    initial coomit

---
+++ src/main/java/org/example/DemoController.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+
+    @GetMapping("/meesage")
+    public String message(){
+        return "Hello from spring boot";
+    }
+}
